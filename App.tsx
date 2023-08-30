3e5657776d3cceeee8655a23859375125c595288
ts7752 <ts7752@naver.com>
2023-08-30 16:07:27 +0900

    기능 추가 : 기기 정보 습득
    기기의 플랫폼 , 해상도 정보를 가져와 파싱하는 함수 개발 완료.

--- App.tsx
+++ App.tsx
@@ -1,16 +1,17 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
+import {
+  currentPlatform,
+  deviceHeight,
+  deviceWidth,
+} from "./Src/Utils/DeviceUtils";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Axios 라이브러리 설치완료</Text>
-      <Text>Navigation 라이브러리 설치 완료</Text>
-      <Text style={{ fontSize: 20, fontWeight: "bold" }}>
-        각 디렉토리 마다 Readme 파일이 존재 할테니 한번씩은 읽어 보시길
-        바랍니다.
-      </Text>
-      <StatusBar style="auto" />
+      <Text> 플랫폼 : {currentPlatform}</Text>
+      <Text> 세로 : {deviceHeight}</Text>
+      <Text> 가로 : {deviceWidth}</Text>
     </View>
   );
 }
