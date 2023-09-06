bceadd3830e0d003f50139a307a0fdefed343da9
Rchaehyeon <gksmf210601@gmail.com>
2023-09-06 16:26:01 +0900

    second commit

---
+++ Src/Screens/Account/SingUp/UniCertiDprtSrch.tsx
@@ -0,0 +1,19 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { RegiCommonView } from "../../../Components/CommonSrc/RegiCommon";
+import { ScreenProps } from "../../../Navigations/StackNavigator";
+
+const UniCertiDprtSrch: React.FC<ScreenProps> = ({ navigation }) => {
+  return (
+    <RegiCommonView
+      bigtext="학번"
+      smalltext="선택하기"
+      inputtext="학번"
+      buttontext="다음"
+      onPress={() => navigation.navigate("TestScreen")}
+      navigation={navigation}
+    />
+  );
+};
+
+export default UniCertiDprtSrch;
