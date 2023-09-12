bb5a01650b25a83c2d0090ba7db0d5d3ea9701db
ArinMiru <hapje010@ptu.ac.kr>
2023-09-12 16:19:16 +0900

    feat/컴포넌트 dir내에 readme 추가, TextStyle추가

--- Src/Styles/TextStyle.tsx
+++ Src/Styles/TextStyle.tsx
@@ -63,4 +63,123 @@ export default StyleSheet.create({
     color: "#ffffff",
     fontSize: 18,
   },
+
+/*-------------------------------------------*/ 
+
+  /*
+    Text Font 모양 
+    { fontWeight: '100' }, // Thin
+    { fontWeight: '200' }, // Ultra Light
+    { fontWeight: '300' }, // Light
+    { fontWeight: '400' }, // Regular
+    { fontWeight: '500' }, // Medium
+    { fontWeight: '600' }, // Semibold
+    { fontWeight: '700' }, // Bold
+    { fontWeight: '800' }, // Heavy
+    { fontWeight: '900' }, // Black
+  */
+
+/*----------------medium---------------------*/ 
+    medium09:{
+      fontFamily: "NotoSans",
+      fontWeight: "500", /*medium*/
+      fontSize: 9,
+    },
+
+    medium12:{
+      fontFamily: "NotoSans",
+      fontWeight: "500", /*medium*/
+      fontSize: 12,
+    },
+
+    medium14:{
+    fontFamily: "NotoSans",
+    fontWeight: "500", /*medium*/
+    fontSize: 14,
+  },
+
+/*----------------semibold-------------------*/ 
+
+  semibold07:{
+    fontFamily: "NotoSans",
+    fontWeight: "600", /*semibold*/
+    fontSize: 7,
+  },
+
+  semibold08:{
+    fontFamily: "NotoSans",
+    fontWeight: "600", /*semibold*/
+    fontSize: 8,
+  },
+
+  semibold10:{
+    fontFamily: "NotoSans",
+    fontWeight: "600", /*semibold*/
+    fontSize: 10,
+  },
+
+  semibold12:{
+    fontFamily: "NotoSans",
+    fontWeight: "600", /*semibold*/
+    fontSize: 12,
+  },
+
+  semibold13:{
+    fontFamily: "NotoSans",
+    fontWeight: "600", /*semibold*/
+    fontSize: 13,
+  },
+
+  semibold14:{
+    fontFamily: "NotoSans",
+    fontWeight: "600", /*semibold*/
+    fontSize: 14,
+  },
+
+/*-----------------bold---------------------*/ 
+
+  bold08:{
+    fontFamily: "NotoSans",
+    fontWeight: "700", /*bold*/
+    fontSize: 8,
+  },
+
+  bold11:{
+    fontFamily: "NotoSans",
+    fontWeight: "700", /*bold*/
+    fontSize: 11,
+  },
+
+  bold12:{
+    fontFamily: "NotoSans",
+    fontWeight: "700", /*bold*/
+    fontSize: 12,
+  },
+
+  /*-------------------------------------------*/ 
+
+  regular07:{
+    fontFamily: "NotoSans",
+    fontWeight: "400", /*regular*/
+    fontSize: 7,
+  },
+
+  regular08:{
+    fontFamily: "NotoSans",
+    fontWeight: "400", /*regular*/
+    fontSize: 8,
+  },
+
+  regular10:{
+    fontFamily: "NotoSans",
+    fontWeight: "400", /*regular*/
+    fontSize: 10,
+  },
+
+  regular13:{
+    fontFamily: "NotoSans",
+    fontWeight: "400", /*regular*/
+    fontSize: 13,
+  },
+
 });
