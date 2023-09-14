e925d909d8bd86bed7d664705d881e34be7fb23f
Rchaehyeon <gksmf210601@gmail.com>
2023-09-14 20:42:11 +0900

    fix/코드 리펙토링
    회원가입과 관련된 UI 코드 리펙토링

--- Src/Screens/Account/SignUp/RegiNmNic.tsx
+++ Src/Screens/Account/SignUp/RegiNmNic.tsx
@@ -1,15 +1,15 @@
 import { View } from "react-native";
 import React from "react";
 import { LoginBackground } from "../../../Components/Reusable/Background";
-import { LongInput } from "../../../Components/Reusable/TextInput";
 import { ScreenProps } from "../../../Navigations/StackNavigator";
+import { IconButton } from "../../../Components/Reusable/Button";
+import { LongButton } from "../../../Components/Reusable/Button";
+import { deviceHeight, deviceWidth } from "../../../Utils/DeviceUtils";
 import { Image } from "react-native";
-import {
-  RegiText2,
-  RegiCommonButton2,
-  RegiNextButton,
-} from "../../../Components/CommonView/CommonCompo";
+import { RegiTextflex1 } from "../../../Components/AccountCompo/AccountText";
+import { RegiDupleFlex2 } from "../../../Components/AccountCompo/AccountCustomCompo";
 import RegiUserData from "../../../Utils/_private/RegiData/RegiUserData";
+import { OnlyAccountInputMarginTop3 } from "../../../Components/AccountCompo/AccoutTextInput";
 
 const RegiNmNic: React.FC<ScreenProps> = ({ navigation }) => {
   // 타입을 명시적으로 설정
@@ -17,16 +17,30 @@ const RegiNmNic: React.FC<ScreenProps> = ({ navigation }) => {
 
   return (
     <LoginBackground>
-      <RegiText2 text="회원가입" />
-      <View style={{ flex: 1, justifyContent: "flex-end" }}>
-        <LongInput text="이름을 입력해주세요." />
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "flex-start",
+          width: deviceWidth * 1,
+        }}
+      >
+        <IconButton
+          text=""
+          onPress={() => navigation.navigate("RegiId")}
+          navigation={navigation}
+        ></IconButton>
       </View>
-      <RegiCommonButton2 inputText="닉네임" text="중복 확인" />
-      <RegiNextButton
-        text="다음"
-        onPress={() => navigation.navigate("RegiPass")}
-        navigation={navigation}
-      />
+      <RegiTextflex1 text="회원가입" />
+      <View style={{ flex: 1 }}>
+        <OnlyAccountInputMarginTop3 text="이름을 입력해주세요." />
+      </View>
+      <RegiDupleFlex2 inputText="닉네임" text="중복 확인" />
+      <View style={{ flex: 4, justifyContent: "flex-start" }}>
+        <LongButton
+          text="다음"
+          onPress={() => navigation.navigate("RegiPass")}
+        />
+      </View>
     </LoginBackground>
   );
 };
--- Src/Styles/AccountInputStyle.tsx
+++ Src/Styles/AccountInputStyle.tsx
@@ -14,16 +14,33 @@ export default StyleSheet.create({
     borderRadius: 11,
     paddingLeft: 10,
     borderColor: "#e8ecf4",
+    backgroundColor: "#f7f8f9",
     borderWidth: 1,
   },
 
   /**
    * /Components/AccountCompo/AccountTextInput
-   * Account에서만 쓰이는 Input
+   * Account에서만 쓰이는 마진 탑이 0.03 들어간 Input
    */
-  onlyAccountInputStyle: {
+  onlyAccountInputStyleMarginTop3: {
     width: deviceWidth * 0.8,
     height: deviceHeight * 0.062,
+    marginTop: deviceHeight * 0.03,
+    backgroundColor: "#f7f8f9",
+    borderRadius: 11,
+    paddingLeft: 10,
+    borderColor: "#e8ecf4",
+    borderWidth: 1,
+  },
+
+  /**
+   * /Components/AccountCompo/AccountTextInput
+   * Account에서만 쓰이는 마진 탑이 0.02 들어간 Input
+   */
+  onlyAccountInputStyleMarginTop2: {
+    width: deviceWidth * 0.8,
+    height: deviceHeight * 0.062,
+    marginTop: deviceHeight * 0.02,
     backgroundColor: "#f7f8f9",
     borderRadius: 11,
     paddingLeft: 10,
