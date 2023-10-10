77a52e3631e9c13c83f283fb4e39133ca3c56087
ts7752 <ts7752@naver.com>
2023-10-10 17:08:02 +0900

    feat / 공지사항 등록 개발 및 버그 수정
    렌더링 호이스팅 버그 해결
    공지사항 게시글 등록 개발

--- Src/Screens/Home/NoTice/NoticePostRegiPage.tsx
+++ Src/Screens/Home/NoTice/NoticePostRegiPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, KeyboardAvoidingView } from "react-native";
 import { AccountBackground } from "../../../Components/AllCompo/Background";
 import { ScreenProps } from "../../../Navigations/StackNavigator";
@@ -13,12 +13,47 @@ import {
   OpenPhotoComboBox,
 } from "../../../Components/ListCompo/OpenCompo/OpenButton";
 import { RegiButton } from "../../../Components/ListCompo/RegiButton";
+import { getUserData } from "../../../Utils/_private/ApiData/UserData";
+import { openBubSvc } from "../../../Services/_private/NoticeApi";
 
 /** [02, 03, 05] TIT_CD에 해당하는 사용자만 접근 가능 페이지 */
 
 const NoticePostRegi: React.FC<ScreenProps> = ({ navigation }) => {
   const [photoButtonClicked, setphotoButtonClicked] = React.useState(false);
+  const [cont, setCont] = useState<string>("");
+  const [tit, setTit] = useState<string>("");
   /** NoticePostRegiPage 컴포넌트에서 사용되는 상태 변수 중 하나인 photoButtonClicked의 값을 변경하는 함수입니다. */
+
+  const handleRegiButtonPress = async () => {
+    try {
+      const userData = getUserData();
+
+      if (userData) {
+        // userData에서 필요한 값을 추출
+        const { LOGIN_ID, MEMB_DEP_CD, MEMB_SC_CD, TIT_CD } = userData;
+
+        // 여기서 사용자가 입력한 값을 서버로 전송
+        // 사용자 입력값과 userData를 사용하여 openBubSvc 호출
+        await openBubSvc(
+          LOGIN_ID,
+          MEMB_DEP_CD,
+          MEMB_SC_CD,
+          TIT_CD,
+          "01", // 아래에서 tit와 cont 값을 어떻게 설정하는지에 따라 변경할 수 있음
+          tit, // 사용자가 입력한 값으로 설정
+          cont // 사용자가 입력한 값으로 설정
+        );
+
+        // 등록 후 필요한 네비게이션 이동 등의 작업 수행
+        // navigation.navigate("다음 화면");
+      } else {
+        console.error("userData가 null입니다.");
+      }
+    } catch (error) {
+      console.error("등록 오류:", error);
+    }
+  };
+
   return (
     <AccountBackground>
       <BackIconTopbarStyle
@@ -33,7 +68,11 @@ const NoticePostRegi: React.FC<ScreenProps> = ({ navigation }) => {
           alignItems: "center",
         }}
       >
-        <OpenFreSgsTitInputBox text="제목을 입력하세요"></OpenFreSgsTitInputBox>
+        <OpenFreSgsTitInputBox
+          text="제목을 입력하세요"
+          value={tit}
+          onChangeText={(text) => setTit(text)}
+        ></OpenFreSgsTitInputBox>
       </View>
       <KeyboardAvoidingView
         style={{
@@ -45,7 +84,11 @@ const NoticePostRegi: React.FC<ScreenProps> = ({ navigation }) => {
         behavior="padding"
         enabled
       >
-        <OpenFreSgsContInputBox text="텍스트를 입력해주세요."></OpenFreSgsContInputBox>
+        <OpenFreSgsContInputBox
+          text="텍스트를 입력해주세요."
+          value={cont}
+          onChangeText={(text) => setCont(text)}
+        ></OpenFreSgsContInputBox>
       </KeyboardAvoidingView>
       <View
         style={{
@@ -71,7 +114,10 @@ const NoticePostRegi: React.FC<ScreenProps> = ({ navigation }) => {
           alignItems: "center",
         }}
       >
-        <RegiButton text="등록하기"></RegiButton>
+        <RegiButton
+          text="등록하기"
+          onPress={handleRegiButtonPress}
+        ></RegiButton>
       </View>
     </AccountBackground>
   );
