255abf6bf57a70278463747e49cce7e9fbc3ee77
JoungEunYu <wjddmsdb124@gmail.com>
2023-10-13 12:09:15 +0900

    feat / 공지사항에 수정삭제닫기 모달 적용

--- Src/Navigations/DrawerNavigator.tsx
+++ Src/Navigations/DrawerNavigator.tsx
@@ -6,7 +6,7 @@ import { ParamListBase } from "@react-navigation/native";
 import SgsPostPage from "../Screens/Community/SuggeStion/SgsPostPage";
 import FrePostPage from "../Screens/Community/Free/FrePostPage";
 import QstPostPage from "../Screens/Community/QuesTion/QstPostPage";
-import MVotePage from "../Screens/Home/VoTe/VotePostPage";
+import VotePostPage from "../Screens/Home/VoTe/VotePostPage";
 import { View } from "react-native";
 import Constants from "expo-constants";
 import {
@@ -50,7 +50,7 @@ const CustomDrawerContent: React.FC<DrawerNavigatorProps> = ({
       <View
         style={{ flex: 1, justifyContent: "flex-end", alignItems: "center" }}
       >
-        <DrawerVoteButton onPress={() => navigation.navigate("공지사항")} />
+        <DrawerVoteButton onPress={() => navigation.navigate("투표")} />
       </View>
       <View
         style={{ flex: 1, justifyContent: "flex-end", alignItems: "center" }}
@@ -107,7 +107,7 @@ function DrawerNavigator({ navigation }: DrawerNavigatorProps) {
       />
       <Drawer.Screen
         name="투표"
-        component={MVotePage}
+        component={VotePostPage}
         options={{ headerShown: false }}
       />
     </Drawer.Navigator>
