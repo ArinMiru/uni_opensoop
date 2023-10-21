caf253832e8203b1fe0701d95194847cf85a1a78
ts7752 <ts7752@naver.com>
2023-10-21 17:34:37 +0900

    fix / 코드 병합
    코드 병합

--- Src/Screens/Home/NoTice/NoticePage.tsx
+++ Src/Screens/Home/NoTice/NoticePage.tsx
@@ -25,6 +25,7 @@ import {
 } from "../../../Components/AllCompo/ModalCompo";
 import NewBackgroundStyle from "../../../Styles/NewBackgroundStyle";
 import { ScreenProps } from "../../../Navigations/StackNavigator";
+import { openBubListDell } from "../../../Services/_private/NoticeApi";
 import { deviceHeight } from "../../../Utils/DeviceUtils";
 
 const NoTicePage: React.FC<ScreenProps> = ({ navigation }) => {
@@ -32,6 +33,7 @@ const NoTicePage: React.FC<ScreenProps> = ({ navigation }) => {
   // 사용자 데이터와 공지사항 데이터 상태를 정의합니다.
   const userData = getUserData(); // 현재 사용자 데이터
   const [sortedData, setSortedData] = useState<NoticeData | null>(null); // 정렬된 공지사항 데이터
+  const [selectedCreSeq, setSelectedCreSeq] = useState<number>(0);
 
   // 컴포넌트가 렌더링될 때 한 번만 실행되는 부분입니다.
   useEffect(() => {
@@ -59,15 +61,21 @@ const NoTicePage: React.FC<ScreenProps> = ({ navigation }) => {
     }
   }, []);
 
-  // 해당 게시글의 CRE_SEQ를 얻기 위한 함수 예시 특정 변수에 저장하는 로직을 변경 하여야 됌 아마도 Utils 디렉토리로 이동할 예정임
-  const handleItemPress = (creSeq: number) => {
-    console.log(creSeq);
-  };
-
   // FlatList 항목들 사이에 구분선을 그리기 위한 함수
   const renderSeparator = () => (
     <View style={{ height: 1, backgroundColor: "#ddd", marginVertical: 4 }} />
   );
+
+  const handleItemPree = (creseq: number) => {
+    setSelectedCreSeq(creseq);
+    console.log(selectedCreSeq);
+    modalFunctions.handleButtonPress();
+  };
+
+  const modalItemDel = () => {
+    openBubListDell(selectedCreSeq);
+    modalFunctions.handleCloseModal();
+  };
 
   /*-------------------------------------------------------------------*/
 
@@ -93,12 +101,11 @@ const NoTicePage: React.FC<ScreenProps> = ({ navigation }) => {
             deviceHeight * 0.25,
             deviceHeight * 0.25,
           ]}
-          enablePanDownToClose={true}
           onDismiss={modalFunctions.handleCloseModal}
         >
           <View style={EditDelCloseModalStyle.contentContainer}>
             <EditModalCompo EditonPress={modalFunctions.handleEditPress} />
-            <DelModalCompo DelonPress={modalFunctions.handleDeletePress} />
+            <DelModalCompo DelonPress={modalItemDel} />
             <CloseModalCompo CloseonPress={modalFunctions.handleCloseModal} />
           </View>
         </BottomSheetModal>
@@ -121,9 +128,8 @@ const NoTicePage: React.FC<ScreenProps> = ({ navigation }) => {
           <FlatList
             data={sortedData?.OPEN_BUB}
             keyExtractor={(item) => item.CRE_SEQ.toString()}
-            renderItem={({ item }) => (
-              <TouchableOpacity onPress={() => handleItemPress(item.CRE_SEQ)}>
-                {/* 해당 게시글의 CRE_SEQ 값을얻기 위한 예시 나중에 바텀시트 나오는 버튼 클릭 시 얻는걸로 변경하여야 됌 지금은 게시글 자체만 클릭해도 얻어 짐*/}
+            renderItem={({ item }) => {
+              return (
                 <NoticePostBoxView
                   MEMB_NM={item.MEMB_NM}
                   MEMB_CD={item.TIT_NM}
@@ -132,10 +138,10 @@ const NoTicePage: React.FC<ScreenProps> = ({ navigation }) => {
                   PostingTime={item.CRE_DAT}
                   postLike={item.LIKE_CNT}
                   PostContent={item.CONT}
-                  onPress={modalFunctions.handleButtonPress}
-                ></NoticePostBoxView>
-              </TouchableOpacity>
-            )}
+                  onPress={() => handleItemPree(item.CRE_SEQ)}
+                />
+              );
+            }}
             ItemSeparatorComponent={renderSeparator}
           />
           {modalFunctions.modalVisible && (
