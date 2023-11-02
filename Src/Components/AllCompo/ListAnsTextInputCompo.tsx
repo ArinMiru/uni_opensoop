ede784db4d75b6190d045f8417d7a47714bc58f3
ArinMiru <hapje010@ptu.ac.kr>
2023-11-03 04:43:42 +0900

    feat / qstAns Modal 개발 및 시간 출력 ux 수정

--- Src/Components/AllCompo/ListAnsTextInputCompo.tsx
+++ Src/Components/AllCompo/ListAnsTextInputCompo.tsx
@@ -59,3 +59,48 @@ export const ListAnsTextInput: React.FC<inputProps> = ({
     </View>
   );
 };
+
+export const QstAnsTextInput: React.FC<inputProps> = ({
+  text,
+  onPress,
+  ...props
+}) => {
+  return (
+    <View
+      style={{
+        height: deviceHeight * 0.1,
+        bottom: deviceHeight * 0.00005,
+        flexDirection: "row-reverse",
+        alignItems: "center",
+        width: "100%",
+        paddingHorizontal: 13,
+        backgroundColor: "#FFFFFF",
+      }}
+    >
+      <TextInput
+        placeholderTextColor="#BDBDBD"
+        style={[QstInputStyles.AnswerInputBoxStyle, textStyle.medium11]}
+        placeholder={"답변을 입력해주세요"}
+        keyboardType="default"
+        {...props}
+      />
+      <View
+        style={{
+          position: "absolute",
+          alignItems: "flex-end",
+        }}
+      >
+        <TouchableOpacity
+          style={{ marginRight: deviceWidth * 0.07 }}
+          onPress={onPress}
+        >
+          <Ionicons
+            name="send-sharp"
+            size={deviceWidth * 0.045}
+            color="#4BB781"
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
